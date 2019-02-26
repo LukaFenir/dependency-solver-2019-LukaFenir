bdbc6e3b2174894b2a4119c9ac55979a1b13cd65
LukaFenir <laura@nichols-online.co.uk>
2019-02-26 22:28:22 +0000

    isValid, missing counter conflicts

--- src/depsolver/Main.java
+++ src/depsolver/Main.java
@@ -2,6 +2,8 @@ package depsolver;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
+import com.sun.xml.internal.bind.v2.runtime.reflect.Lister;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -10,6 +12,45 @@ import java.util.List;
 
 public class Main {
 
+    public static void bruteForce(){
+
+    }
+
+    public static boolean isValid(List<Package> state){
+        if(state.isEmpty()){
+            return true;
+        }
+        Package x = state.get(state.size()-1); //last element
+        List<List<Package>> deps = x.getDependsExpanded(); // [B3.2,C][D]
+        if(!deps.isEmpty()) {
+            //depsAreInState
+            //for each List, is at least 1 package in state?
+            for(List<Package> dep : deps){
+                if(!atLeastOne(state, dep)) { //If even one dep is not satisfied
+                    return false;
+                }
+            }
+        }
+        List<Package> confs = x.getConflictsExpanded();
+        if(!confs.isEmpty()) {
+            for(Package pack : confs){
+                if(state.contains(pack)){
+                    return false; //Conflicting package is in state
+                }
+            }
+        }
+        return true;
+    }
+
+    public static boolean atLeastOne(List<Package> state, List<Package> dep){
+        for(Package pack : dep){
+            if(state.contains(pack)){
+                return true; //Package is in state
+            }
+        }
+        return false; //No package was in state
+    }
+
     public static void main(String[] args) throws IOException {
     TypeReference<List<Package>> repoType = new TypeReference<List<Package>>() {};
     List<Package> repo = JSON.parseObject(readFile(args[0]), repoType);
@@ -17,12 +58,22 @@ public class Main {
     List<String> initial = JSON.parseObject(readFile(args[1]), strListType);
     List<String> constraints = JSON.parseObject(readFile(args[2]), strListType);
 
+
+
     List<Package> initialState = new ArrayList<>();
 
     // Go through each package and parse string constraints into Package references
     for(Package pack : repo) {
         pack.expandRepoConstraints(repo);
     }
+    List<Package> testState = new ArrayList<>();
+    /*testState.add(repo.get(3)); //C
+    testState.add(repo.get(4)); //D
+    testState.add(repo.get(0)); //A -> C/B3.2 and D*/
+    testState.add(repo.get(3)); //C
+    testState.add(repo.get(2)); //B3.0
+    Boolean x = isValid(testState);
+
     FinalConstraints finalConstraints = new FinalConstraints(constraints,repo);
 
     //Take a final constraint, check it for children
